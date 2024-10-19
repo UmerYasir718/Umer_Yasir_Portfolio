11d0c74977b9b9015bee3b1ecbb9fe2320989297
UmerYasir718 <umeryasir718@gmail.com>
2024-10-19 14:33:05 +0500

    route testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import MainRoutes from "./routes";
+import Header from "./components/Header";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import MyResume from "./Pages/MyResume";
+import Portfolio from "./Pages/Portfolio";
 
 function App() {
-  return <MainRoutes />;
+  return <>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/Resume' element={<MyResume />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Router></>;
 }
 
 export default App;
